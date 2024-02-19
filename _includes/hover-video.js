bddec2d0518d8f487069da3f9fcf11321390b4bd
gira <gira.patel94@gmail.com>
2024-02-19 13:42:48 -0500

    updates
    - fixed copy on about page
    - fixed hover video code so videos autoplay on mobile

--- _includes/hover-video.js
+++ _includes/hover-video.js
@@ -1,13 +1,26 @@
-// pause hover video effect
-const hoverVideos = document.getElementsByClassName('hoverVideo');
+    // Check if it's a mobile device
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-// Iterate over each element with the class 'hoverVideo'
-Array.from(hoverVideos).forEach(video => {
-  video.addEventListener('mouseover', () => {
-    video.play();
-  });
+    // pause hover video effect
+    const hoverVideos = document.getElementsByClassName('hoverVideo');
 
-  video.addEventListener('mouseout', () => {
-    video.pause();
-  });
-});
+    // Iterate over each element with the class 'hoverVideo'
+    Array.from(hoverVideos).forEach(video => {
+      // Check if it's a mobile device
+      if (isMobile) {
+        // Add autoplay attribute for mobile devices
+        video.setAttribute('autoplay', '');
+      } else {
+        // Add hover event listeners for non-touch devices
+        video.addEventListener('mouseover', () => {
+          video.play();
+        });
+
+        video.addEventListener('mouseout', () => {
+          video.pause();
+        });
+      }
+    });
+
+
+
